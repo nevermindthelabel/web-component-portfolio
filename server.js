d44ffea1adaec95afd40727d6191fd912a08bcf1
Matt Kilcup <nevermindthelabel@gmail.com>
2020-05-22 21:45:18 -0700

    updated server

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('express works');
+});
+
 app.listen(PORT, () => console.log(`App 👂 at http://localhost:${PORT}`));
