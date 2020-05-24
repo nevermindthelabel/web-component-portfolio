e2d89a01516dc10de59f98422273edf4ec68f085
Matt Kilcup <nevermindthelabel@gmail.com>
2020-05-24 15:56:27 -0700

    updated styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,25 @@ import { css, html, LitElement } from 'lit-element';
 
 export class Navbar extends LitElement {
 
+  static get styles() {
+    return css`
+      .navbar {
+        overflow: hidden;
+        background-color: #f1f1f1;
+        padding: 90px 10px;
+        transition: .4s;
+        position: fixed;
+        width: 100%;
+        top: 0;
+      }
+    `;
+  }
+
   render() {
     return html`
-      <h1>navbar works</h1>
+    <div class="navbar">
+      <a href="#" class="brand">Matt Kilcup</a>
+    </div>
     `
   }
 }
