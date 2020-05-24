ef12acc66ab1009cf6e470ff8493e61e7007de1a
Matt Kilcup <nevermindthelabel@gmail.com>
2020-05-24 15:38:59 -0700

    initial navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import { css, html, LitElement } from 'lit-element';
+
+export class Navbar extends LitElement {
+
+  render() {
+    return html `
+      <h1>navbar works</h1>
+    `
+  }
+}
+
+customElements.define('portfolio-navbar', Navbar);
