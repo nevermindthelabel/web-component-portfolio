15a250cd3dcdcc359f38618c423f24406e07a264
Matt Kilcup <nevermindthelabel@gmail.com>
2020-06-04 20:51:23 -0700

    updated css

--- src/components/cards.js
+++ src/components/cards.js
@@ -7,6 +7,7 @@ export class Cards extends LitElement {
         .card {
           box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
           transition: 0.3s;
+          height: 300px;
         }
         .card:hover {
           box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
