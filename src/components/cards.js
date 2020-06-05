826f931c22902038f86465ba5f8756d5884d0d2b
Matt Kilcup <nevermindthelabel@gmail.com>
2020-06-04 20:49:01 -0700

    updated card component

--- src/components/cards.js
+++ src/components/cards.js
@@ -5,7 +5,14 @@ export class Cards extends LitElement {
     return [
       css `
         .card {
-
+          box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+          transition: 0.3s;
+        }
+        .card:hover {
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        }
+        .container {
+          padding: 2px 16px;
         }
       `
     ]
@@ -14,6 +21,8 @@ export class Cards extends LitElement {
   render() {
     return html`
       <div class="card">
+        <div class="container">
+        </div>
       </div>
     `
   }
