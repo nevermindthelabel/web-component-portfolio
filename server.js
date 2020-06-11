0985670eb9b13856e17495a25d5bee3215b3b346
Matt Kilcup <nevermindthelabel@gmail.com>
2020-06-10 21:28:32 -0700

    added api routes to server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
+const routes = require('./routes');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('express works');
 });
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`App 👂 at http://localhost:${PORT}`));
