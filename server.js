1e67ed18a52b96e3b986e987144e30b711532dec
Matt Kilcup <nevermindthelabel@gmail.com>
2020-06-26 22:03:31 -0700

    moved routes into routes folter

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const routes = require('./routes');
 
 const app = express();
 
-app.get('/', async (req, res) => {
-  await res.send('express works');
-});
-
-app.get('/test', async (req, res) => {
-  await res.send('testing route works');
-});
-
 app.use(routes);
 
 app.listen(PORT, () => console.log(`App 👂 at http://localhost:${PORT}`));
