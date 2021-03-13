39ff39e7bbf2b5365536a04e6c2b3ad667c39ddd
Matt Kilcup <nevermindthelabel@gmail.com>
2021-03-13 10:31:06 -0700

    added check for node env

--- server/server.js
+++ server/server.js
@@ -6,4 +6,8 @@ const app = express();
 
 app.use(routes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(PORT, () => console.log(`App 👂 at http://localhost:${PORT}`));
